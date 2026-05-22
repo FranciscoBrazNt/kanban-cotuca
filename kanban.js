efc411a957490f7625f2366e1bb7f69265b5a170
FranciscoBrazNt <jovemjovemx@gmail.com>
2026-05-22 19:38:46 -0300

    Update kanban.js

--- kanban.js
+++ kanban.js
@@ -1,29 +1,16 @@
 const formulario = document.getElementById("CriarKanban")
 const enviar = document.getElementById("SubmitKanban")
 
-let nomes = [];
-let descricoes = [""];
-let prazos = [];
-let situacao = [];
-let prioridade = [];
-let listaDeAtividades = [];
+
+let listaDeAtividades = JSON.parse(localStorage.getItem("minhasTarefas")) || [];
 const DestinoFazer = document.getElementById("KanbansFazer");
 const DestinoProgresso = document.getElementById("KanbansProgresso");
 const DestinoConcluido = document.getElementById("KanbansConcluido");
+const BodyCardF = document.getElementsByClassName("Kanbanzinho")
 
 
 
 
-for (let i = 0; i < nomes.length; i++) {
-    let novoCard = {
-        nome: nomes[i],
-        descricao: descricoes[i],
-        prazo: prazos[i],
-        status: situacao[i],
-        prioridade: prioridade[i],
-    }
-    listaDeAtividades.push(novoCard)
-}
 
 function MudarStatusProgressivo(idCard) {
     let Card = listaDeAtividades.find(item => item.id === idCard)
@@ -31,6 +18,7 @@ function MudarStatusProgressivo(idCard) {
     if (Card.status === "Fazer") { Card.status = "Progresso" }
     else if (Card.status === "Progresso") { Card.status = "Feito" }
     else { Card.status = "Fazer" }
+    localStorage.setItem("minhasTarefas", JSON.stringify(listaDeAtividades));
     DesenharCards();
 }
 
@@ -38,6 +26,7 @@ function MudarStatusRetrogrado(idCard) {
     let Card = listaDeAtividades.find(item => item.id === idCard)
     if (Card.status === "Progresso") { Card.status = "Fazer" }
     else { Card.status = "Progresso" }
+    localStorage.setItem("minhasTarefas", JSON.stringify(listaDeAtividades));
     DesenharCards();
 }
 
@@ -55,28 +44,36 @@ function DesenharCards() {
 
 
     for (let i = 0; i < CardFazer.length; i++) {
+        let cor = ObterCor(CardFazer[i].prioridade)
         DestinoFazer.innerHTML += `
-            <div class="Kanbanzinho" id="KbF${i}">
+            <div class="Kanbanzinho" id="KbF${i}" style="border-left:3px solid ${cor}">
                 <div class="HeaderKanbanzinho" id="HdF${i}">
                     <h3 class="NomeKanbanzinho"> ${CardFazer[i].nome} </h3>
                 </div>
                 <div class="BodyKanbanzinho">
                     <h3 class="descKanbanzinho"> ${CardFazer[i].descricao} </h3>
-                    <h3 class="prazoKanbanzinho"> ${CardFazer[i].prazo} Dias </h3>
-                    <button class="ButtonKanbanzinho" id="ButtonP${CardFazer[i]}" type="button" onclick="MudarStatusProgressivo(${CardFazer[i].id})">Começar Atividade</button>
+                    <div class="PrazoePrioridade">
+                        <h3 class="Prioridade"> Prioridade: ${CardFazer[i].prioridade}</h3>
+                        <h3 class="prazoKanbanzinho"> ${CardFazer[i].prazo} Dias </h3>
+                    </div>
+                    <button class="ButtonKanbanzinho" id="ButtonP${CardFazer[i].id}" type="button" onclick="MudarStatusProgressivo(${CardFazer[i].id})">Começar Atividade</button>
                 </div>
             </div>`;
     }
 
     for (let i = 0; i < CardProgresso.length; i++) {
+        let cor = ObterCor(CardProgresso[i].prioridade)
         DestinoProgresso.innerHTML += `
-            <div class="KanbanzinhoP" id="KbP${i}">
+            <div class="KanbanzinhoP" id="KbP${i}" style="border-left:3px solid ${cor}">
                 <div class="HeaderKanbanzinhoP" id="HdP${i}">
                     <h3 class="NomeKanbanzinho"> ${CardProgresso[i].nome} </h3>
                 </div>
                 <div class="BodyKanbanzinhoP">
                     <h3 class="descKanbanzinho"> ${CardProgresso[i].descricao} </h3>
-                    <h3 class="prazoKanbanzinho"> ${CardProgresso[i].prazo} Dias </h3>
+                    <div class="PrazoePrioridade">
+                         <h3 class="Prioridade"> Prioridade: ${CardProgresso[i].prioridade}</h3>
+                        <h3 class="prazoKanbanzinho"> ${CardProgresso[i].prazo} Dias </h3>
+                    </div>
                     <div class="ContainerBotoes">
                         <button 
                             class="ButtonKanbanzinhoP"
@@ -94,14 +91,18 @@ function DesenharCards() {
             </div>`;
     }
     for (let i = 0; i < CardFeito.length; i++) {
+        let cor = ObterCor(CardFeito[i].prioridade)
         DestinoConcluido.innerHTML += `
-            <div class="KanbanzinhoC" id="KbC${i}">
-                <div class="HeaderKanbanzinhoC" id="HdC${i}">
-                    <h3 class="NomeKanbanzinho"> ${CardFeito[i].nome} </h3>
+            <div class="KanbanzinhoC" id="KbC${i}" style="border-left: solid ${cor}">
+                <div class="HeaderKanbanzinhoC" id="HdC${i}" >
+                    <h3 class="NomeKanbanzinho" style="text-decoration: line-through; text-decoration-thickness: 3px;"> ${CardFeito[i].nome} </h3>
                 </div>
                 <div class="BodyKanbanzinhoC">
                     <h3 class="descKanbanzinho"> ${CardFeito[i].descricao} </h3>
-                    <h3 class="prazoKanbanzinho"> ${CardFeito[i].prazo} Dias </h3>
+                    <div class="PrazoePrioridade">
+                        <h3 class="Prioridade"> Prioridade: ${CardFeito[i].prioridade}</h3>
+                        <h3 class="prazoKanbanzinho"> ${CardFeito[i].prazo} Dias </h3>
+                    </div>
 
                     <div class="ContainerBotoes">
                         <button 
@@ -139,15 +140,13 @@ function adicionarCard(e) {
         }
         listaDeAtividades.push(novoCard);
         formulario.reset();
+        localStorage.setItem("minhasTarefas", JSON.stringify(listaDeAtividades));
         DesenharCards();
+        
     }
     
 
 }
-
-
-
-
 
 function mostrarForms() {
     if (formulario.style.display == "flex") { formulario.style.display = "none" }
@@ -157,9 +156,18 @@ function mostrarForms() {
 function ExcluirCard(cardId){
     listaDeAtividades = listaDeAtividades.filter(item => item.id !== cardId)
     DesenharCards();
+    localStorage.setItem("minhasTarefas", JSON.stringify(listaDeAtividades));
 }
+
+function ObterCor(prioridade){
+    if (prioridade === "Baixa") return "#5ac05aff"
+    if (prioridade === "Media") return "#f8d340ff"
+    return "#d34c4cff"
+}
+
 
 let addKanban = document.getElementById("add-kanban")
 addKanban.addEventListener("click", mostrarForms)
 enviar.addEventListener("click", adicionarCard)
 
+DesenharCards()
